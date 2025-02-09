4fe4154f34bac18c65acd7b48c301df1f09fe0ac
lenolnbamm <lenoln1997@gmail.com>
2025-02-08 21:08:22 -0300

    first commit

---
+++ src/test/java/br/com/projedata/iniflex/IniflexApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projedata.iniflex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IniflexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
